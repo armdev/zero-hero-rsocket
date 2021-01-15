4560fef8191e85660bb139156c8ec7ce160f7a0d
Armen Arzumanyan <armena@YLT160001.corp.optym.net>
2021-01-15 13:50:18 +0400

    The Next Best Code

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker rm -f $(sudo docker ps -a -q)
-docker rmi $(sudo docker images | grep "^<none>" | awk "{print $3}")
+docker rm -f $(docker ps -a -q)
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
 
 docker images
