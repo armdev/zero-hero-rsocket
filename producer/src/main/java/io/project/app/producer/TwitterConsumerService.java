7ba054f98149de78e1515c227c69336767da7f55
root <root@YLT160001.corp.optym.net>
2021-01-16 17:06:19 +0400

    The Next Best Code

--- producer/src/main/java/io/project/app/producer/TwitterConsumerService.java
+++ producer/src/main/java/io/project/app/producer/TwitterConsumerService.java
@@ -1,34 +1,32 @@
 package io.project.app.producer;
 
 import com.project.tw.domain.StatusModel;
+import java.time.Duration;
+import static java.time.Duration.ofSeconds;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import reactor.core.publisher.Flux;
 
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
+import static reactor.util.retry.Retry.backoff;
 
 @Service
 @Slf4j
 public class TwitterConsumerService {
 
-    private final Mono<RSocketRequester> requesterMono;
+    private final RSocketRequester requesterMono;
 
     public TwitterConsumerService(RSocketRequester.Builder builder) {
         this.requesterMono = builder
                 .dataMimeType(MediaType.APPLICATION_CBOR)
-                .connectTcp("twitter", 2051).retry(5).cache();
+                .tcp("twitter", 2051);
     }
 
     public Flux<StatusModel> get() {
-        log.info("Connecting with twitter");
-        return this.requesterMono
-                .flatMapMany(req
-                        -> req.route("find.stream")
-                        .data("NODATA")
-                        .retrieveFlux(StatusModel.class))
-                .take(4);
+        return this.requesterMono.route("find.stream")
+                .data("NODATA")
+                .retrieveFlux(StatusModel.class).take(Duration.ZERO).cache(Duration.ZERO).retryWhen(backoff(5, ofSeconds(1)).maxBackoff(ofSeconds(20)));
 
     }
 
